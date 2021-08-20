4eeebec7c8a8920202585f48313f4b2dc85a8be4
Brian <blee0613@gmail.com>
2021-08-19 20:28:56 -0700

    icons, basic form tutorial

---
+++ pages/components/ThemeSwitcher.js
@@ -0,0 +1,13 @@
+import { IconButton, useColorMode } from '@chakra-ui/react'
+import { SunIcon, MoonIcon } from '@chakra-ui/icons'
+
+export default function ThemeSwitcher() {
+  const { colorMode, toggleColorMode } = useColorMode()
+  return (
+    <IconButton
+      aria-label="Toggle Theme"
+      onClick={toggleColorMode}
+      icon={colorMode === 'dark' ? <MoonIcon /> : <SunIcon />}>
+    </IconButton>
+)
+}
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,32 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import ThemeSwitcher from './components/ThemeSwitcher.js'
 import styles from '../styles/Home.module.css'
+import { Flex, Heading, FormControl, Button, Input, useColorModeValue } from '@chakra-ui/react'
 
 export default function Home() {
+  const formBackground = useColorModeValue('gray.100', 'gray.700');
+  const inputBackground = useColorModeValue('gray.100', 'gray.700')
   return (
-    <div>
-
-    </div>
+    <>
+    <Flex><ThemeSwitcher /></Flex>
+    <Flex
+      height="100vh"
+      justify="center"
+      align="center">
+      <Flex
+        p={12}
+        rounded={6}
+        backgroundColor={formBackground}
+        direction="column">
+        <Heading mb={3}>Login</Heading>
+        <FormControl>
+          <Input variant="filled" type="email" placeholder="email" mb={3}></Input>
+          <Input variant="filled" type="password" placeholder="password" mb={3}></Input>
+          <Button color="white" backgroundColor="teal" mb={3}>Login</Button>
+        </FormControl>
+      </Flex>
+    </Flex>
+    </>
   )
 }
